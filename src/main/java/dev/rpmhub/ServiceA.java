a05c11210816bdec9e41ef99d9c57687beac25a9
Dafne <dafne.makrakis@hotmail.com>
2022-10-18 19:50:06 -0300

    pw2-rest

---
+++ src/main/java/dev/rpmhub/ServiceA.java
@@ -0,0 +1,27 @@
+
+package dev.rpmhub;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+@Path("/servicea")
+public class ServiceA {
+
+    @Inject
+    @RestClient
+    RemoteInterface service;
+
+    @GET
+    @Path("/person/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person getPerson(@PathParam("name") String name){
+        return service.getPerson(name);
+    }
+
+}
